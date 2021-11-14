ec0fefbb080c251c183170fcc39c9f64e139fe52
umuttaskiran <umut.taskiran7@gmail.com>
2021-11-14 13:52:27 +0300

    add query into AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,6 @@
 <script>
+import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
+
 export default {
   name: "AppHeader",
   data() {
@@ -6,6 +8,39 @@ export default {
       selectedValue: "User",
       selectItems: ["User", "Admin"],
     };
+  },
+  methods: {
+    ...mapActions(["getSymbolDataFromApi"]),
+    ...mapMutations(["SET_INPUT_VALUE", "SET_CURRENT_SYMBOL_DATA"]),
+    async getSymbolsPage() {
+      await this.getSymbolDataFromApi(this.daily);
+      this.SET_CURRENT_SYMBOL_DATA(this.symbolDatas.dailyDataSet);
+      this.$router.push({
+        name: "SymbolPage",
+        params: {
+          id: this.inputValue,
+        },
+      });
+    },
+    getHome() {
+      this.$router.push({
+        name: "Home",
+      });
+    },
+  },
+  computed: {
+    value: {
+      get() {
+        return this.inputValue;
+      },
+      set(value) {
+        this.SET_INPUT_VALUE(value);
+      },
+    },
+    ...mapState(["inputValue", "symbolDatas"]),
+    ...mapGetters({
+      daily: "_getDailyQuery",
+    }),
   },
 };
 </script>
@@ -15,12 +50,14 @@ export default {
     <v-app-bar app dark color="secondary" height="90" class="header-container">
       <v-row class="justify-center align-center">
         <v-col sm="3">
-          <i class="fas fa-hand-holding-usd fa-3x"></i>
+          <span @click="getHome" style="cursor: pointer"
+            ><i class="fas fa-hand-holding-usd fa-3x"></i
+          ></span>
         </v-col>
         <v-col sm="6">
           <v-row class="justify-center">
             <v-col sm="10">
-              <v-form>
+              <v-form @submit.prevent="getSymbolsPage">
                 <v-text-field
                   v-model="value"
                   placeholder="Search symbol, ex: TSLA"
