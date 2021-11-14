e06c3c7dbdc6fba409a88fce81928ffb76595937
umuttaskiran <umut.taskiran7@gmail.com>
2021-11-14 23:08:17 +0300

    update Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,30 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>Home Page</h1>
+  <div class="text-wrapper">
+    <div>
+      <p class="fs-60 text-center">Welcome to Stock Market App</p>
+      <p class="fs-35 text-center">
+        You can search for symbols using the input in the header.
+      </p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.fs-60 {
+  font-size: 60px;
+}
+
+.fs-35 {
+  font-size: 35px;
+}
+
+.text-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+}
+/* https://www.mc-tr.com/attachments/krito-para-borsa-scripti-png.147886/ */
+</style>
