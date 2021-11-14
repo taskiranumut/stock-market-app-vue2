555a23916af5b801e3bbc45f5a660e5faeb1a608
umuttaskiran <umut.taskiran7@gmail.com>
2021-11-14 19:09:53 +0300

    update LogPanels

--- src/components/LogPanels.vue
+++ src/components/LogPanels.vue
@@ -1,19 +1,39 @@
 <script>
+import { mapState } from "vuex";
 export default {
   name: "LogPanels",
+  computed: {
+    ...mapState(["log"]),
+  },
 };
 </script>
 
 <template>
   <v-expansion-panels>
-    <v-expansion-panel v-for="(item, i) in 5" :key="i">
-      <v-expansion-panel-header> Item </v-expansion-panel-header>
+    <v-expansion-panel v-for="(logData, i) in log" :key="i">
+      <v-expansion-panel-header
+        :class="{ 'color-red': logData.isUnauthorized }"
+      >
+        <h3>Log {{ i + 1 }}</h3>
+      </v-expansion-panel-header>
       <v-expansion-panel-content>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        <p class="m-0"><strong>Time: </strong> {{ logData.time }}</p>
+        <p class="m-0"><strong>From: </strong> {{ logData.from }}</p>
+        <p class="m-0"><strong>To: </strong> {{ logData.to }}</p>
+        <p class="m-0">
+          <strong>Is Authorized: </strong> {{ !logData.isUnauthorized }}
+        </p>
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
+
+<style scoped>
+.color-red {
+  color: #ff0000;
+}
+
+.m-0 {
+  margin: 0;
+}
+</style>
