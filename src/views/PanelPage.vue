fb6623c9423dea6683abb7b6c66beab780df9842
umuttaskiran <umut.taskiran7@gmail.com>
2021-11-14 17:09:07 +0300

    add LogPanels component to PanelPage

--- src/views/PanelPage.vue
+++ src/views/PanelPage.vue
@@ -1,11 +1,21 @@
 <script>
+import LogPanels from "@/components/LogPanels.vue";
+
 export default {
   name: "PanelPage",
+  components: {
+    LogPanels,
+  },
 };
 </script>
 
 <template>
-  <div>
-    <h1>Panel Page</h1>
-  </div>
+  <v-container>
+    <v-row class="justify-center align-center">
+      <v-col sm="8">
+        <h1 class="my-5">Recorded Logs</h1>
+        <LogPanels />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
